7b48cf2e4615e76037eda6e4c979a600c3192e87
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2021-12-24 01:12:43 +0500

    Left Right turn added

--- Problem-Slover/Assets/Scripts/Blocks.cs
+++ Problem-Slover/Assets/Scripts/Blocks.cs
@@ -6,18 +6,33 @@ public class Blocks : MonoBehaviour
 {
     public void MoveForward(Transform t)
     {
-        t.position = t.position + new Vector3(0f, 5f, 0f);
+        t.position = t.position + new Vector3(0f, 10f, 0f);
     } 
     public void MoveBack(Transform t)
     {
-        t.position = t.position - new Vector3(0f, 5f, 0f);
+        t.position = t.position - new Vector3(0f, 10f, 0f);
     }
     public void RightMove(Transform t)
     {
-        t.position = t.position + new Vector3(5f, 0f, 0f);
+        t.position = t.position + new Vector3(10f, 0f, 0f);
     }
     public void leftMove(Transform t)
     {
-        t.position = t.position - new Vector3(5f, 0f, 0f);
+        t.position = t.position - new Vector3(10f, 0f, 0f);
+    }
+    public void TurnLeft(Transform t)
+    {
+        t.Rotate(Vector3.forward, 90 );
+    }
+    public void TurnRight(Transform t)
+    {
+        t.Rotate(Vector3.back, 90 );
+    }
+    public void LoopFunction()
+    {
+        for (int i =0; i < 5;i++)
+        {
+            //Call Function 
+        }
     }
 }
