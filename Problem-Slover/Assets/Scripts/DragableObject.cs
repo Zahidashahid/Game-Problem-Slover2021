96299abea5c02003f540e7013585ba3712cd8d95
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2021-12-28 00:16:17 +0500

    Draggable Btn

---
+++ Problem-Slover/Assets/Scripts/DragableObject.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragableObject : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+    RectTransform rectTransorm;
+    public Canvas canvas;
+    public CanvasGroup canvasGroup;
+    private void Awake()
+    {
+        rectTransorm = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false;
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        rectTransorm.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
